2664d1be2c53edaa616259cbab8f9e9665acb825
Bishamoda <mihailshipakin@mail.ru>
2020-05-21 08:51:33 +0300

    laba3

---
+++ off/off.bat
@@ -0,0 +1,26 @@
+echo off
+md Shipakin
+cd Shiapkin
+md Mihail
+cd Mihail
+md Alekseevich
+cd ..
+pause
+echo> 23041998.txt
+cd Mihail
+cd Alekseevich
+echo> asus.txt
+cd ..
+cd ..
+cd ..
+pause
+del Shipakin /S/Q/F
+pause
+cd Shipakin
+cd Mihail
+rd Alekseevich
+cd ..
+rd Mihail
+cd ..
+rd Shipakin
+pause
